bd22e4a528188a697182ec14dd71be98f79d80c3
Milka Putri <milka.putri@ti.ukdw.ac.id>
2024-11-30 11:16:24 +0700

    Foodiefy App

---
+++ app/src/main/java/com/example/foodiefy_halaman_awal_aplikasi/foodiefy_sign_up.kt
@@ -0,0 +1,20 @@
+package com.example.foodiefy_halaman_awal_aplikasi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class foodiefy_sign_up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_foodiefy_sign_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
